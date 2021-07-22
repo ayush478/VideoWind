0483e4ba77fa12a9fad320d7a0c021ac92470aad
Ayush Sunariya <69513008+ayush478@users.noreply.github.com>
2021-07-22 15:28:46 +0530

    connected frontend with beckend

--- index.js
+++ index.js
@@ -1,45 +1,36 @@
-const express = require("express");
-const app = express();
+const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
-const {
-    resolve4
-} = require("dns");
-const {
-    disconnect
-} = require("process");
 
 const io = require("socket.io")(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
+
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    res.send("server is running");
-})
-
-io.on("connection", (socket) => {
-	socket.emit("me", socket.id);
-
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
-	});
-
-	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
-		io.to(userToCall).emit("callUser", { signal: signalData, from, name });
-	});
-
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
-	});
+  res.send("Running");
 });
 
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
 
-server.listen(PORT, () => {
-    console.log(`server is listening on port ${PORT}`);
-})
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("callEnded");
+  });
+
+  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("callUser", { signal: signalData, from, name });
+  });
+
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+});
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
