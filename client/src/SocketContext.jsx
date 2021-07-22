524abfb75f9502a31832adcd07a6d5339b8b6afe
Ayush Sunariya <69513008+ayush478@users.noreply.github.com>
2021-07-22 20:01:44 +0530

    deploy

--- client/src/SocketContext.jsx
+++ client/src/SocketContext.jsx
@@ -5,7 +5,7 @@ import Peer from 'simple-peer';
 const SocketContext = createContext();
 
 // const socket = io('http://localhost:5000');
-const socket = io(' https://react-video-webrtc.herokuapp.com/');
+const socket = io(' https://react-video-webrtc.herokuapp.com');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
